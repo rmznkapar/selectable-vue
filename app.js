041b30463892ab459ad6f507b32ae9bfac2c4100
rmznkapar <rmznkapar@gmail.com>
2018-04-10 20:52:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var selectable_app = new Vue({
 el: "#selectable-box",
 data: {
-	selectableItems: ["Archie","Veronica","Betty","Jughead","Cheryl"],
+	selectableItems: {
+		0:"Archie",
+		1:"Veronica",
+		2:"Betty",
+		3:"Jughead",
+		4:"Cheryl"
+	},
+	selectedItems: [false,false,false,false,false],
 	isSelecting:false,
 	bzStartX: 0,
 	bzStartY: 0,
@@ -12,24 +19,23 @@ data: {
 },
 methods:{
     onSelecting(event) {
-		console.log(event.clientX);
-		console.log(event.clientY);
-
 		this.bzStartX = event.clientX;
 		this.bzStartY = event.clientY;
 
 		this.bzLeftX = event.clientX;
 		this.bzTopY = event.clientY;
 
+		this.selectedItems = [false,false,false,false,false];
 		this.isSelecting = true;
 
     },
     outSelecting(event) {
-		console.log(event.clientX);
-		console.log(event.clientY);
+   		this.getPosition();
 		this.isSelecting = false;
 		this.bzWidthX = 0;
-		this.bzHeightY = 0;
+		this.bzHeightY = 0;	
+
+
     },
     mouseMove(event) {
 		if (this.isSelecting) { 
@@ -46,7 +52,26 @@ methods:{
 				this.bzLeftX = event.clientX ;
 			}
 		}
+
+    },
+    getPosition(){
+		var div = document.getElementsByClassName("bzBlock");
+    	for (var i = 0; i < div.length; i++) {
+	    	if ( 
+	    		(this.bzLeftX + this.bzWidthX) > div[i].offsetLeft && 
+	    		(this.bzTopY + this.bzHeightY) > div[i].offsetTop  &&
+	    		(this.bzTopY) < (div[i].offsetTop + div[i].offsetHeight) &&
+				(this.bzLeftX) < (div[i].offsetLeft + div[i].offsetWidth)
+	    	) {
+	    		this.selectedItems[i] = true;
+	    	}
+    	}
+    	console.log(this.selectedItems);
+    },
+    clickSelecting(event) {
+
     }
+
 }
 
 }) 
